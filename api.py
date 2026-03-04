4f0c3257e54321546ca6abb827f1322abb7a7322
大神 <xuchen@babuyo.com>
2026-03-04 18:28:16 +0800

    不启动日报

--- api.py
+++ api.py
@@ -198,35 +198,35 @@ def get_review_stats():
 
 @api_app.route('/review/daily_report', methods=['GET'])
 def daily_report():
-    # 获取当前日期0点和23点59分59秒的时间戳
-    start_time = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0).timestamp()
-    end_time = datetime.now().replace(hour=23, minute=59, second=59, microsecond=0).timestamp()
+    # # 获取当前日期0点和23点59分59秒的时间戳
+    # start_time = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0).timestamp()
+    # end_time = datetime.now().replace(hour=23, minute=59, second=59, microsecond=0).timestamp()
 
-    try:
-        if push_review_enabled:
-            df = ReviewService().get_push_review_logs(updated_at_gte=start_time, updated_at_lte=end_time)
-        else:
-            df = ReviewService().get_mr_review_logs(updated_at_gte=start_time, updated_at_lte=end_time)
+    # try:
+    #     if push_review_enabled:
+    #         df = ReviewService().get_push_review_logs(updated_at_gte=start_time, updated_at_lte=end_time)
+    #     else:
+    #         df = ReviewService().get_mr_review_logs(updated_at_gte=start_time, updated_at_lte=end_time)
 
-        if df.empty:
-            logger.info("No data to process.")
-            return jsonify({'message': 'No data to process.'}), 200
-        # 去重：基于 (author, message) 组合
-        df_unique = df.drop_duplicates(subset=["author", "commit_messages"])
-        # 按照 author 排序
-        df_sorted = df_unique.sort_values(by="author")
-        # 转换为适合生成日报的格式
-        commits = df_sorted.to_dict(orient="records")
-        # 生成日报内容
-        report_txt = Reporter().generate_report(json.dumps(commits))
-        # 发送钉钉通知
-        notifier.send_notification(content=report_txt, msg_type="markdown", title="代码提交日报")
+    #     if df.empty:
+    #         logger.info("No data to process.")
+    #         return jsonify({'message': 'No data to process.'}), 200
+    #     # 去重：基于 (author, message) 组合
+    #     df_unique = df.drop_duplicates(subset=["author", "commit_messages"])
+    #     # 按照 author 排序
+    #     df_sorted = df_unique.sort_values(by="author")
+    #     # 转换为适合生成日报的格式
+    #     commits = df_sorted.to_dict(orient="records")
+    #     # 生成日报内容
+    #     report_txt = Reporter().generate_report(json.dumps(commits))
+    #     # 发送钉钉通知
+    #     notifier.send_notification(content=report_txt, msg_type="markdown", title="代码提交日报")
 
-        # 返回生成的日报内容
-        return json.dumps(report_txt, ensure_ascii=False, indent=4)
-    except Exception as e:
-        logger.error(f"Failed to generate daily report: {e}")
-        return jsonify({'message': f"Failed to generate daily report: {e}"}), 500
+    #     # 返回生成的日报内容
+    #     return json.dumps(report_txt, ensure_ascii=False, indent=4)
+    # except Exception as e:
+    #     logger.error(f"Failed to generate daily report: {e}")
+    #     return jsonify({'message': f"Failed to generate daily report: {e}"}), 500
 
 
 def setup_scheduler():
